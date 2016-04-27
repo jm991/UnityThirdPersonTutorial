jm991/UnityThirdPersonTutorial

commit 132200f2d3166a4bebf81ab2b93db8696b45cbba

Fixed regression from previous commit
Arrow reappears when unlocking while still in range

Assets/Scripts/ThirdPersonCamera.cs
@@ -121,7 +121,7 @@ public class ThirdPersonCamera : MonoBehaviour
 	private Vector3 lookDir;
 	private Vector3 curLookDir;
 	private BarsEffect barEffect;
-	private CamStates camState = CamStates.Behind;	
+    private CamStates camState = CamStates.Behind;	
 	private float xAxisRot = 0.0f;
 	private CameraPosition firstPersonCamPos;			
 	private float lookWeight;
@@ -137,6 +137,10 @@ public class ThirdPersonCamera : MonoBehaviour
 	private Vector3 characterOffset;
 	private Vector3 targetPosition;	
     private Vector3 lookAt;
+
+    // Variables for explicitly setting camera modes
+    private CamStates forcedCamState = CamStates.Behind;  
+    private bool forceCamState = false;
 	
 	#endregion
 	
@@ -331,7 +335,12 @@ public class ThirdPersonCamera : MonoBehaviour
 		if (lShiftKeyDown)
 		{
 			leftTrigger = 1;
-		}
+        }
+        if (leftTrigger <= TARGETING_THRESHOLD)
+        {
+            // Once trigger is let up again, we can reset this variable
+            targetingSystem.ForceUnlock = false;
+        }
 		
 		characterOffset = followXform.position + (distanceUp * followXform.up);
 		
@@ -340,50 +349,59 @@ public class ThirdPersonCamera : MonoBehaviour
 
 		// Determine camera state
 		// * Targeting *
-		if (leftTrigger > TARGETING_THRESHOLD)
-		{			
-			if (camState != CamStates.Target) 
-			{
-                savedRigToGoal = Vector3.zero;
+        /*if (forceCamState)
+        {
+            camState = forcedCamState;
+            forceCamState = false;
+        } 
+        else*/
+        {
+            if (leftTrigger > TARGETING_THRESHOLD && !targetingSystem.ForceUnlock)
+            {			
+                if (camState != CamStates.Target)
+                {
+                    savedRigToGoal = Vector3.zero;
 
-                // If there is a target displayed on screen when entering this mode, we need to lock on it
-                //targetingSystem.Lock();
-			}
-			camState = CamStates.Target;
+                    // If there is a target displayed on screen when entering this mode, we need to lock on it
+                    //targetingSystem.Lock();
+                }
+                camState = CamStates.Target;
 
-			barEffect.coverage = Mathf.SmoothStep(barEffect.coverage, widescreen, targetingTime);
-			follow.Animator.SetLayerWeight ((int)AnimatorLayers.Targeting, 1.0f);
-		}
-		else
-		{	
-			barEffect.coverage = Mathf.SmoothStep(barEffect.coverage, 0f, targetingTime);
-            follow.Animator.SetLayerWeight ((int)AnimatorLayers.Targeting, 0.0f);
-            lookAt = characterOffset;
+                barEffect.coverage = Mathf.SmoothStep (barEffect.coverage, widescreen, targetingTime);
+                follow.Animator.SetLayerWeight ((int)AnimatorLayers.Targeting, 1.0f);
+            } 
+            else
+            {	
+                barEffect.coverage = Mathf.SmoothStep (barEffect.coverage, 0f, targetingTime);
+                follow.Animator.SetLayerWeight ((int)AnimatorLayers.Targeting, 0.0f);
+                lookAt = characterOffset;
 			
-			// * First Person *
-			if (rightY > firstPersonThreshold && camState != CamStates.Free && !follow.IsInLocomotion())
-			{
-				// Reset look before entering the first person mode
-				xAxisRot = 0;
-				lookWeight = 0f;
-				camState = CamStates.FirstPerson;
-			}
+                // * First Person *
+                if (rightY > firstPersonThreshold && camState != CamStates.Free && !follow.IsInLocomotion ())
+                {
+                    // Reset look before entering the first person mode
+                    xAxisRot = 0;
+                    lookWeight = 0f;
+                    camState = CamStates.FirstPerson;
+                }
 
-			// * Free *
-			if ((rightY < freeThreshold || mouseWheel < 0f) && System.Math.Round(follow.Speed, 2) == 0)
-			{
-				camState = CamStates.Free;
-				savedRigToGoal = Vector3.zero;
-                savedRigToGoalDirection = RigToGoalDirection;
-			}
+                // * Free *
+                if ((rightY < freeThreshold || mouseWheel < 0f) && System.Math.Round (follow.Speed, 2) == 0)
+                {
+                    camState = CamStates.Free;
+                    savedRigToGoal = Vector3.zero;
+                    savedRigToGoalDirection = RigToGoalDirection;
+                }
 
-			// * Behind the back *
-			if ((camState == CamStates.FirstPerson && bButtonPressed) || 
-				(camState == CamStates.Target && leftTrigger <= TARGETING_THRESHOLD))
-			{
-				camState = CamStates.Behind;
-			}
-		}
+                // * Behind the back *
+                if ((camState == CamStates.FirstPerson && bButtonPressed) ||
+                    (camState == CamStates.Target && leftTrigger <= TARGETING_THRESHOLD) ||
+                    targetingSystem.ForceUnlock)
+                {
+                    camState = CamStates.Behind;
+                }
+            }
+        }
 		
 		// Execute camera state
 		switch (camState)
@@ -621,8 +639,19 @@ public class ThirdPersonCamera : MonoBehaviour
 	
 	#endregion
 	
+
+    #region Methods (public)
+
+    public void ForceCameraState(CamStates forcedCamState)
+    {
+        this.forcedCamState = forcedCamState;
+        forceCamState = true;
+    }
+
+    #endregion
 	
-	#region Methods
+
+    #region Methods (private)
 	
 	private void SmoothPosition(Vector3 fromPos, Vector3 toPos)
 	{		

Assets/Scenes/OutsetIsland.unity
@@ -1168,6 +1168,7 @@ MonoBehaviour:
   appearTrigger: Locked
   disappearTrigger: Disappear
   disappearForcedTrigger: DisappearForced
+  appearForcedTrigger: AppearForced
   lockingAnimation: Locking
   appearAnimation: Appear
   disappearAnimation: Disappear
@@ -1427,7 +1428,7 @@ Prefab:
       objectReference: {fileID: 0}
     - target: {fileID: 11400006, guid: e6e40eda5a3f31244baa1160274612a8, type: 2}
       propertyPath: startingState
-      value: 2
+      value: 0
       objectReference: {fileID: 0}
     - target: {fileID: 11400006, guid: e6e40eda5a3f31244baa1160274612a8, type: 2}
       propertyPath: distanceAway
@@ -1710,15 +1711,15 @@ Prefab:
     m_Modifications:
     - target: {fileID: 400140, guid: 79efd4c2b57a6d24f9555a5851c00ad1, type: 2}
       propertyPath: m_LocalPosition.x
-      value: 40.69
+      value: 37.99
       objectReference: {fileID: 0}
     - target: {fileID: 400140, guid: 79efd4c2b57a6d24f9555a5851c00ad1, type: 2}
       propertyPath: m_LocalPosition.y
-      value: 5.69
+      value: 5.11
       objectReference: {fileID: 0}
     - target: {fileID: 400140, guid: 79efd4c2b57a6d24f9555a5851c00ad1, type: 2}
       propertyPath: m_LocalPosition.z
-      value: 167.17
+      value: 159.13
       objectReference: {fileID: 0}
     - target: {fileID: 400140, guid: 79efd4c2b57a6d24f9555a5851c00ad1, type: 2}
       propertyPath: m_LocalRotation.x
@@ -1726,7 +1727,7 @@ Prefab:
       objectReference: {fileID: 0}
     - target: {fileID: 400140, guid: 79efd4c2b57a6d24f9555a5851c00ad1, type: 2}
       propertyPath: m_LocalRotation.y
-      value: -0.5836728
+      value: -0.34828442
       objectReference: {fileID: 0}
     - target: {fileID: 400140, guid: 79efd4c2b57a6d24f9555a5851c00ad1, type: 2}
       propertyPath: m_LocalRotation.z
@@ -1734,7 +1735,7 @@ Prefab:
       objectReference: {fileID: 0}
     - target: {fileID: 400140, guid: 79efd4c2b57a6d24f9555a5851c00ad1, type: 2}
       propertyPath: m_LocalRotation.w
-      value: 0.81198895
+      value: 0.93738896
       objectReference: {fileID: 0}
     - target: {fileID: 400140, guid: 79efd4c2b57a6d24f9555a5851c00ad1, type: 2}
       propertyPath: m_RootOrder

Assets/UI/Targeting/TargetingYellow.anim
@@ -24,7 +24,7 @@ AnimationClip:
         inSlope: 0
         outSlope: 0
         tangentMode: 10
-      - time: 0.33333334
+      - time: 0.083333336
         value: 1
         inSlope: 0
         outSlope: 0
@@ -40,14 +40,14 @@ AnimationClip:
       serializedVersion: 2
       m_Curve:
       - time: 0
-        value: 0
-        inSlope: 3
-        outSlope: 3
-        tangentMode: 10
-      - time: 0.33333334
         value: 1
-        inSlope: 3
-        outSlope: 3
+        inSlope: 0
+        outSlope: 0
+        tangentMode: 10
+      - time: 0.083333336
+        value: 1
+        inSlope: 0
+        outSlope: 0
         tangentMode: 10
       m_PreInfinity: 2
       m_PostInfinity: 2
@@ -64,7 +64,7 @@ AnimationClip:
         inSlope: 0
         outSlope: 0
         tangentMode: 10
-      - time: 0.33333334
+      - time: 0.083333336
         value: 0
         inSlope: 0
         outSlope: 0
@@ -80,14 +80,14 @@ AnimationClip:
       serializedVersion: 2
       m_Curve:
       - time: 0
-        value: 1
-        inSlope: 0
-        outSlope: 0
+        value: 0
+        inSlope: 12
+        outSlope: 12
         tangentMode: 10
-      - time: 0.33333334
+      - time: 0.083333336
         value: 1
-        inSlope: 0
-        outSlope: 0
+        inSlope: 12
+        outSlope: 12
         tangentMode: 10
       m_PreInfinity: 2
       m_PostInfinity: 2
@@ -105,7 +105,7 @@ AnimationClip:
   m_ClipBindingConstant:
     genericBindings:
     - path: 0
-      attribute: 2998903001
+      attribute: 1541146092
       script: {fileID: 11500000, guid: be3cebbd78221e8478d3152ceb405e15, type: 3}
       classID: 114
       customType: 0
@@ -117,13 +117,13 @@ AnimationClip:
       customType: 0
       isPPtrCurve: 0
     - path: 0
-      attribute: 3268760662
+      attribute: 2998903001
       script: {fileID: 11500000, guid: be3cebbd78221e8478d3152ceb405e15, type: 3}
       classID: 114
       customType: 0
       isPPtrCurve: 0
     - path: 0
-      attribute: 1541146092
+      attribute: 3268760662
       script: {fileID: 11500000, guid: be3cebbd78221e8478d3152ceb405e15, type: 3}
       classID: 114
       customType: 0
@@ -134,7 +134,7 @@ AnimationClip:
     m_AdditiveReferencePoseClip: {fileID: 0}
     m_AdditiveReferencePoseTime: 0
     m_StartTime: 0
-    m_StopTime: 0.33333334
+    m_StopTime: 0.083333336
     m_OrientationOffsetY: 0
     m_Level: 0
     m_CycleOffset: 0
@@ -158,7 +158,7 @@ AnimationClip:
         inSlope: 0
         outSlope: 0
         tangentMode: 10
-      - time: 0.33333334
+      - time: 0.083333336
         value: 1
         inSlope: 0
         outSlope: 0
@@ -174,14 +174,14 @@ AnimationClip:
       serializedVersion: 2
       m_Curve:
       - time: 0
-        value: 0
-        inSlope: 3
-        outSlope: 3
-        tangentMode: 10
-      - time: 0.33333334
         value: 1
-        inSlope: 3
-        outSlope: 3
+        inSlope: 0
+        outSlope: 0
+        tangentMode: 10
+      - time: 0.083333336
+        value: 1
+        inSlope: 0
+        outSlope: 0
         tangentMode: 10
       m_PreInfinity: 2
       m_PostInfinity: 2
@@ -198,7 +198,7 @@ AnimationClip:
         inSlope: 0
         outSlope: 0
         tangentMode: 10
-      - time: 0.33333334
+      - time: 0.083333336
         value: 0
         inSlope: 0
         outSlope: 0
@@ -214,14 +214,14 @@ AnimationClip:
       serializedVersion: 2
       m_Curve:
       - time: 0
-        value: 1
-        inSlope: 0
-        outSlope: 0
+        value: 0
+        inSlope: 12
+        outSlope: 12
         tangentMode: 10
-      - time: 0.33333334
+      - time: 0.083333336
         value: 1
-        inSlope: 0
-        outSlope: 0
+        inSlope: 12
+        outSlope: 12
         tangentMode: 10
       m_PreInfinity: 2
       m_PostInfinity: 2

commit 50a43b9e6202ed331da3d9f2c30ecc6b7d1e10a3

Almost done! Camera looks between player and target and releasing when out of range works again
- Camera now lookAt between the target and the player, similar to how it
works in Wind Waker (previously, it just pointed at the target)
- Fixed a bug (apparently I never implemented or regressed the feature
from commit 132200f?) where moving out of the targeting distance no
longer unlocked the camera properly to return back to Behind CamState
-- Fixed by routing the Disappear animation state to the DisappearForced
state
-- All of the forceCamState and forcedCamState variables in
ThirdPersonCamera seem to be unused in favor of TargetingSystem's
ForceUnlock, so I commented them out
- Fixed a bug where sometimes quickly choosing a new target didn't make
the arrow appear by routing "DisappearForced" to "Red" via "Locked"
trigger

Assets/Scripts/TargetingSystem.cs
@@ -235,6 +235,7 @@ public class TargetingSystem : MonoBehaviour
             if (currentTarget == null)
             {
                 Debug.Log ("null target", this);
+                //currentTarget = visibleTargets [0];
             }
             // Position the cursor above the closest target
             this.transform.position = currentTarget.transform.position + new Vector3 (0, currentTarget.GetComponent<Collider> ().bounds.size.y);
@@ -289,7 +290,7 @@ public class TargetingSystem : MonoBehaviour
                 // If we're locked, unlock and switch back to Behind CamState when we don't have a target in range any longer
                 Unlock ();
                 Debug.Log ("Unlocked auto from locked state - behind cam", this);
-                gamecam.ForceCameraState (ThirdPersonCamera.CamStates.Behind);
+                // gamecam.ForceCameraState (ThirdPersonCamera.CamStates.Behind);
             } 
             else if (currentTarget == null)
             {

Assets/Scripts/ThirdPersonCamera.cs
@@ -146,8 +146,8 @@ public class ThirdPersonCamera : MonoBehaviour
     private float lookAtDampingThreshold = 0.3f;
 
     // Variables for explicitly setting camera modes
-    private CamStates forcedCamState = CamStates.Behind;  
-    private bool forceCamState = false;
+    // private CamStates forcedCamState = CamStates.Behind;  
+    // private bool forceCamState = false;
     private float lastLeftTrigger = 0.0f;
 
     [SerializeField]
@@ -384,7 +384,12 @@ public class ThirdPersonCamera : MonoBehaviour
             }
         }
 
-        if (leftTrigger > (1.0f - TARGETING_THRESHOLD) && !targetingSystem.ForceUnlock)
+        /*if (forceCamState)
+        {
+            camState = forcedCamState;
+            forceCamState = false;
+        } */
+        if (leftTrigger > (1.0f - TARGETING_THRESHOLD) && !targetingSystem.ForceUnlock && !targetingSystem.ForceUnlock)
         {			
             TargetingSetup();
 
@@ -553,8 +558,9 @@ public class ThirdPersonCamera : MonoBehaviour
                     // targetPosition = halfwayPoint + RigToGoalDirection * distanceAway;
 
                     //targetLookAt = targetingSystem.CurrentTarget.transform.position;
-                    lookAt = targetingSystem.CurrentTarget.transform.position;
-                    lookAt.y += distanceUp;
+                    //lookAt = targetingSystem.CurrentTarget.transform.position;
+                    lookAt = halfwayPoint;
+                    //lookAt.y += distanceUp;
                     //lookAt = right;
 
                     //targetPosition = characterOffset + followXform.up - (targetingSystem.CurrentTarget.transform.position - followXform.position) * distanceAway;
@@ -688,11 +694,11 @@ public class ThirdPersonCamera : MonoBehaviour
 
     #region Methods (public)
 
-    public void ForceCameraState(CamStates forcedCamState)
+    /*public void ForceCameraState(CamStates forcedCamState)
     {
         this.forcedCamState = forcedCamState;
         forceCamState = true;
-    }
+    }*/
 
     #endregion
 	

Assets/UI/Targeting/Targeting.controller
@@ -360,6 +360,30 @@ AnimatorStateTransition:
   m_InterruptionSource: 0
   m_OrderedInterruption: 1
   m_CanTransitionToSelf: 1
+--- !u!1101 &110173662
+AnimatorStateTransition:
+  m_ObjectHideFlags: 1
+  m_PrefabParentObject: {fileID: 0}
+  m_PrefabInternal: {fileID: 0}
+  m_Name: 
+  m_Conditions:
+  - m_ConditionMode: 1
+    m_ConditionEvent: DisappearForced
+    m_EventTreshold: 0
+  m_DstStateMachine: {fileID: 0}
+  m_DstState: {fileID: 110230492}
+  m_Solo: 0
+  m_Mute: 0
+  m_IsExit: 0
+  serializedVersion: 3
+  m_TransitionDuration: 0.25
+  m_TransitionOffset: 0
+  m_ExitTime: 0.57142854
+  m_HasExitTime: 1
+  m_HasFixedDuration: 1
+  m_InterruptionSource: 0
+  m_OrderedInterruption: 1
+  m_CanTransitionToSelf: 1
 --- !u!1101 &110179178
 AnimatorStateTransition:
   m_ObjectHideFlags: 1
@@ -376,6 +400,30 @@ AnimatorStateTransition:
   m_TransitionDuration: 0.25
   m_TransitionOffset: 0
   m_ExitTime: 0.6666666
+  m_HasExitTime: 1
+  m_HasFixedDuration: 1
+  m_InterruptionSource: 0
+  m_OrderedInterruption: 1
+  m_CanTransitionToSelf: 1
+--- !u!1101 &110179812
+AnimatorStateTransition:
+  m_ObjectHideFlags: 1
+  m_PrefabParentObject: {fileID: 0}
+  m_PrefabInternal: {fileID: 0}
+  m_Name: 
+  m_Conditions:
+  - m_ConditionMode: 1
+    m_ConditionEvent: Locked
+    m_EventTreshold: 0
+  m_DstStateMachine: {fileID: 0}
+  m_DstState: {fileID: 110202396}
+  m_Solo: 0
+  m_Mute: 0
+  m_IsExit: 0
+  serializedVersion: 3
+  m_TransitionDuration: 0.25
+  m_TransitionOffset: 0
+  m_ExitTime: 0.75
   m_HasExitTime: 1
   m_HasFixedDuration: 1
   m_InterruptionSource: 0
@@ -535,6 +583,7 @@ AnimatorState:
   m_CycleOffset: 0
   m_Transitions:
   - {fileID: 110167360}
+  - {fileID: 110179812}
   m_StateMachineBehaviours: []
   m_Position: {x: 50, y: 50, z: 0}
   m_IKOnFeet: 0
@@ -585,6 +634,7 @@ AnimatorState:
   - {fileID: 110169730}
   - {fileID: 110136488}
   - {fileID: 110165682}
+  - {fileID: 110173662}
   m_StateMachineBehaviours: []
   m_Position: {x: 50, y: 50, z: 0}
   m_IKOnFeet: 0

UnityThirdPersonTutorial.userprefs
@@ -1,31 +1,37 @@
 ﻿<Properties StartupItem="Assembly-CSharp.csproj" RefactoringSettings.EnableRefactorings="True">
   <MonoDevelop.Ide.Workspace ActiveConfiguration="Debug" PreferredExecutionTarget="Unity.Instance.Unity Editor" />
-  <MonoDevelop.Ide.Workbench ActiveDocument="Assets\Scripts\TargetingSystem.cs">
+  <MonoDevelop.Ide.Workbench ActiveDocument="Assets\Scripts\ThirdPersonCamera.cs">
     <Files>
-      <File FileName="Assets\Scripts\ThirdPersonCamera.cs" Line="106" Column="5" />
+      <File FileName="Assets\Scripts\ThirdPersonCamera.cs" Line="365" Column="33" />
       <File FileName="Assets\Scripts\SceneFadeInOut.cs" Line="1" Column="1" />
-      <File FileName="Assets\Scripts\CharacterControllerLogic.cs" Line="334" Column="10" />
-      <File FileName="Assets\Scripts\TargetingSystem.cs" Line="183" Column="25" />
+      <File FileName="Assets\Scripts\CharacterControllerLogic.cs" Line="191" Column="68" />
+      <File FileName="Assets\Scripts\TargetingSystem.cs" Line="292" Column="38" />
       <File FileName="Assets\Scripts\AnimatorHelper.cs" Line="1" Column="1" />
-      <File FileName="Assets\Scripts\Targetable.cs" Line="7" Column="34" />
+      <File FileName="Assets\Scripts\Targetable.cs" Line="19" Column="74" />
       <File FileName="Assets\Scripts\Sight.cs" Line="1" Column="1" />
       <File FileName="Assets\Scripts\Tags.cs" Line="1" Column="1" />
       <File FileName="Assets\Scripts\LastPlayerSighting.cs" Line="1" Column="1" />
+      <File FileName="Assets\Scripts\NPCSight.cs" Line="104" Column="92" />
+      <File FileName="Assets\Scripts\AnimateShader.cs" Line="1" Column="1" />
     </Files>
   </MonoDevelop.Ide.Workbench>
   <MonoDevelop.Ide.DebuggingService.Breakpoints>
     <BreakpointStore>
-      <Breakpoint enabled="false" file="C:\Users\john\Documents\GitHub\UnityThirdPersonTutorial\Assets\Scripts\TargetingSystem.cs" line="85" column="45" />
-      <Breakpoint enabled="false" file="C:\Users\john\Documents\GitHub\UnityThirdPersonTutorial\Assets\Scripts\TargetingSystem.cs" line="149" column="1" />
-      <Breakpoint file="C:\Users\john\Documents\GitHub\UnityThirdPersonTutorial\Assets\Scripts\TargetingSystem.cs" line="327" column="1" />
-      <Breakpoint file="C:\Users\john\Documents\GitHub\UnityThirdPersonTutorial\Assets\Scripts\TargetingSystem.cs" line="317" column="1" />
-      <Breakpoint file="C:\Users\john\Documents\GitHub\UnityThirdPersonTutorial\Assets\Scripts\TargetingSystem.cs" line="211" column="82" />
-      <Breakpoint file="C:\Users\john\Documents\GitHub\UnityThirdPersonTutorial\Assets\Scripts\TargetingSystem.cs" line="177" column="37" />
-      <Breakpoint file="C:\Users\john\Documents\GitHub\UnityThirdPersonTutorial\Assets\Scripts\TargetingSystem.cs" line="183" column="37" />
+      <Breakpoint enabled="false" file="C:\Users\john\Documents\GitHub\UnityThirdPersonTutorial\Assets\Scripts\TargetingSystem.cs" line="86" column="45" />
+      <Breakpoint file="C:\Users\john\Documents\GitHub\UnityThirdPersonTutorial\Assets\Scripts\TargetingSystem.cs" line="405" column="1" />
+      <Breakpoint file="C:\Users\john\Documents\GitHub\UnityThirdPersonTutorial\Assets\Scripts\TargetingSystem.cs" line="390" column="1" />
+      <Breakpoint file="C:\Users\john\Documents\GitHub\UnityThirdPersonTutorial\Assets\Scripts\TargetingSystem.cs" line="395" column="1" />
+      <Breakpoint file="C:\Users\john\Documents\GitHub\UnityThirdPersonTutorial\Assets\Scripts\TargetingSystem.cs" line="321" column="1" />
+      <Breakpoint file="C:\Users\john\Documents\GitHub\UnityThirdPersonTutorial\Assets\Scripts\TargetingSystem.cs" line="349" column="1" />
+      <Breakpoint enabled="false" file="C:\Users\john\Documents\GitHub\UnityThirdPersonTutorial\Assets\Scripts\TargetingSystem.cs" line="307" column="1" />
+      <Breakpoint file="C:\Users\john\Documents\GitHub\UnityThirdPersonTutorial\Assets\Scripts\TargetingSystem.cs" line="345" column="1" />
+      <Breakpoint file="C:\Users\john\Documents\GitHub\UnityThirdPersonTutorial\Assets\Scripts\ThirdPersonCamera.cs" line="421" column="42" />
     </BreakpointStore>
   </MonoDevelop.Ide.DebuggingService.Breakpoints>
   <MonoDevelop.Ide.DebuggingService.PinnedWatches>
     <Watch file="Assets\Scripts\TargetingSystem.cs" line="96" offsetX="263" offsetY="1520" expression="targets" liveUpdate="False" />
-    <Watch file="Assets\Scripts\TargetingSystem.cs" line="320" offsetX="662" offsetY="5104" expression="firstUntargeted" liveUpdate="False" />
+    <Watch file="Assets\Scripts\TargetingSystem.cs" line="318" offsetX="514" offsetY="5098" expression="gamecam.CamState" liveUpdate="False" />
+    <Watch file="Assets\Scripts\TargetingSystem.cs" line="318" offsetX="531" offsetY="5131" expression="locked" liveUpdate="False" />
+    <Watch file="Assets\Scripts\TargetingSystem.cs" line="345" offsetX="274" offsetY="5490" expression="HasTarget" liveUpdate="False" />
   </MonoDevelop.Ide.DebuggingService.PinnedWatches>
 </Properties>